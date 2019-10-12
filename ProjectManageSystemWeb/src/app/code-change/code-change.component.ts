740c0f7c9a18f038a18db0694b79a4a32a2b7bd7
804173948 <804173948@qq.com>
2019-10-12 16:57:58 +0800

    Personal information frontend

---
+++ ProjectManageSystemWeb/src/app/code-change/code-change.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-code-change',
+  templateUrl: './code-change.component.html',
+  styleUrls: ['./code-change.component.css']
+})
+export class CodeChangeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
